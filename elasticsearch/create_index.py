3706115a43c9f970b0f3d6e53f66ffa558c7801c
CValle_ <claravallegmz@gmail.com>
2025-11-20 14:56:24 +0100

    .scrapy folder removed

--- elasticsearch/create_index.py
+++ elasticsearch/create_index.py
@@ -6,27 +6,28 @@ es = Elasticsearch("http://localhost:9200")
 INDEX_NAME = "pisos_index"
 
 # Mapping adaptado con embedding y campos de pisos
-mapping = {
-    "mappings": {
-        "properties": {
-            "title": {"type": "text"},
-            "description": {"type": "text"},
-            "category": {"type": "keyword"},
-            "price": {"type": "float"},
-            "neighborhood": {"type": "keyword"},
-            "rooms": {"type": "integer"},
-            "bathrooms": {"type": "integer"},
-            "surface_m2": {"type": "float"},
-            "floor": {"type": "keyword"},
-            "has_elevator": {"type": "boolean"},
-            "is_exterior": {"type": "boolean"},
-            "phone": {"type": "keyword"},
-            "published_at": {"type": "date"},
-            "latitude": {"type": "float"},
-            "longitude": {"type": "float"},
-            "images": {"type": "keyword"}
-        }
+{
+  "mappings": {
+    "properties": {
+      "url": { "type": "keyword" },
+      "listing_id": { "type": "keyword" },
+      "price_eur": { "type": "float" },
+
+      "title": { "type": "text" },
+
+      "rooms": { "type": "integer" },
+      "bathrooms": { "type": "integer" },
+      "surface_m2": { "type": "float" },
+      "floor": { "type": "keyword" },
+
+      "has_elevator": { "type": "boolean" },
+      "is_exterior": { "type": "boolean" },
+
+      "neighborhood": { "type": "keyword" },
+
+      "images": { "type": "keyword" }
     }
+  }
 }
 
 # def create_index():
